93405568a75dac5774e77fed90c0085e4bcf2083
Mujahid Fisher <fishermujahid@gmail.com>
2024-11-18 13:28:10 +0200

    alot of work

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="landing-comp">
+    <div class="landing-comp-left">
+      <div class="heading-1">
+        <h1 class="hello-msg">
+          Hello,<br />
+          I am Mujahid
+        </h1>
+      </div>
+      <div class="heading-2">
+        <h2 class="occupation">Web Developer/Designer</h2>
+      </div>
+      <div class="paragraph-1">
+        <p class="about-intro">
+          I am seeking a career not a job in a field closely<br>
+          related to the world of technology, as that is<br> 
+          what I have set my life’s goals towards. I believe<br> 
+          the future holds a bright path in the tech<br>
+          industry.
+        </p>
+      </div>
+      <div class="btn-1">
+        <button class="view-cv">View CV</button>
+      </div>
+    </div>
+    <div class="landing-comp-right">
+      <div class="profile-pic">
+        <img src="../assets/profilepic.png" alt="profileImage" />
+      </div>
+    </div>
+    <div class="element-shape-1"></div>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style></style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,7 +8,7 @@
       </div>
       <div class="nav-center">
         <div class="nav-item">
-          <router-link to="/home" class="nav-link">Home</router-link>
+          <router-link to="/" class="nav-link">Home</router-link>
 
         </div>
       </div>
